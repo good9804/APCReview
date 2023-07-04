c758735887d11e1a1133611d0bba168e78531be4
Korjw <fkrk56@naver.com>
2023-07-04 13:22:00 +0900

    2023_07_04_03

--- review.js
+++ review.js
@@ -12,8 +12,8 @@ app.post("/upload", async (req, res) => {
   new_review = new Review({
     review_id: req.body.results[2].response,
     review_item: req.body.results[3].response,
-    review_reaction: req.body.results[6].response,
-    others: req.body.results[7].response,
+    review_reaction: req.body.results[4].response,
+    others: req.body.results[5].response,
   });
   //DB 연결 후 리뷰 저장
   await new_review.save();
@@ -25,7 +25,6 @@ app.get("/kind", async (req, res) => {
 
   var send_product_list = [];
   product_list.forEach((element) => {
-    console.log(element);
     send_product_list.push({
       product_item: element.product_item,
       product_detail: element.product_detail,
